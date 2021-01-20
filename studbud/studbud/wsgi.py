77c11c79705c4a04d823da8e64dbbba5d1734152
Jessica Shi <jessicashi605@gmail.com>
2021-01-20 02:00:42 -0500

    fix studbud

--- studbud/studbud/wsgi.py
+++ studbud/studbud/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/Users/junpingshi/GitStudBud/StudBud-Site/studbud') 
+sys.path.append('/Users/junpingshi/GitStudBud/StudBud-Site/studbud/studbud')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studbud.settings')
 
 application = get_wsgi_application()
