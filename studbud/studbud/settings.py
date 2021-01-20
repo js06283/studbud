9fd56b4624d0bcb63ca0bd0a5e7b421d2346f55a
Jessica Shi <jessicashi605@gmail.com>
2021-01-20 01:24:32 -0500

    add packages

--- studbud/studbud/settings.py
+++ studbud/studbud/settings.py
@@ -137,3 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+import django_heroku
+django_heroku.settings(locals())
