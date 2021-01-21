cfead2555ae07fbdd302b169edd5f74036253860
Jessica Shi <jessicashi605@gmail.com>
2021-01-21 00:07:02 -0500

    logging

--- studbud/settings.py
+++ studbud/settings.py
@@ -143,3 +143,19 @@ DATABASES['default'].update(prod_db)
 
 import django_heroku
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
