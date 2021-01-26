296c755d4ec5506f2dd0c5a79af5b83d3a4ab16c
Sameer Saxena <sameerdsaxena@gmail.com>
2021-01-26 12:34:57 -0500

    Added whitenoise compression for static file storage

--- studbud/settings.py
+++ studbud/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = (
 )
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
