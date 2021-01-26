67847eb16d99ddb7ed50b518d3d2045734586bb8
Sameer Saxena <sameerdsaxena@gmail.com>
2021-01-26 04:22:03 -0500

    Collect static files

--- studbud/settings.py
+++ studbud/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
